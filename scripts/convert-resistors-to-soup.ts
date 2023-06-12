07c8228b46d4f240fb6d7e6fa90d7491e28f4c63
Severin Ibarluzea <seve700@gmail.com>
2023-06-12 02:48:12 +0000

    wip

--- scripts/convert-resistors-to-soup.ts
+++ scripts/convert-resistors-to-soup.ts
@@ -3,8 +3,22 @@ import { parseEagleXML } from "@tscircuit/eagle-xml-converter"
 
 async function main(){
   const resistorXml = (await fs.readFile("./tmp-sparkfun-repo/SparkFun-Resistors.lbr")).toString()
+  
+  const parsedEagle = parseEagleXML(resistorXml)
+  
+  await fs.writeFile("tmp/parsed-eagle.json", JSON.stringify(parsedEagle,null,"  "))
+  
+  const numToLayerNameMap = {}
+  
+  for (const layer of parsedEagle.layers){
+    numToLayerNameMap[layer.number] = layer.name.toUpperCase()
+  }
+  
+  console.log(numToLayerNameMap)
     
-  console.log(parseEagleXML(resistorXml).library.packages[0])
+  const comp = parsedEagle.library.packages[0]
+  
+  console.log(comp)
 
 }
 
