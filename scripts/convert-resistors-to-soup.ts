486c61cd95de1b816950926207b078ee0b17aa06
Severin Ibarluzea <seve700@gmail.com>
2023-06-12 05:48:10 +0000

    style(convert-resistors-to-soup): Add semicolons and whitespace for readability

--- scripts/convert-resistors-to-soup.ts
+++ scripts/convert-resistors-to-soup.ts
@@ -1,25 +1,31 @@
-import * as fs from "fs/promises"
-import { parseEagleXML } from "@tscircuit/eagle-xml-converter"
+import * as fs from "fs/promises";
+import { parseEagleXML } from "@tscircuit/eagle-xml-converter";
 
-async function main(){
-  const resistorXml = (await fs.readFile("./tmp-sparkfun-repo/SparkFun-Resistors.lbr")).toString()
-  
-  const parsedEagle = parseEagleXML(resistorXml)
-  
-  await fs.writeFile("tmp/parsed-eagle.json", JSON.stringify(parsedEagle,null,"  "))
-  
-  const numToLayerNameMap = {}
-  
-  for (const layer of parsedEagle.layers){
-    numToLayerNameMap[layer.number] = layer.name.toUpperCase()
+async function main() {
+  const resistorXml = (
+    await fs.readFile("./tmp-sparkfun-repo/SparkFun-Resistors.lbr")
+  ).toString();
+
+  const parsedEagle = parseEagleXML(resistorXml);
+
+  await fs.writeFile(
+    "tmp/parsed-eagle.json",
+    JSON.stringify(parsedEagle, null, "  ")
+  );
+
+  const numToLayerNameMap = {};
+
+  const top_layer = parsedEagle.layers.find((layer) => layer.name === "Top");
+
+  for (const layer of parsedEagle.layers) {
+    numToLayerNameMap[layer.number] = layer.name.toUpperCase();
   }
-  
-  console.log(numToLayerNameMap)
-    
-  const comp = parsedEagle.library.packages[0]
-  
-  console.log(comp)
 
+  // console.log(numToLayerNameMap);
+
+  const comp = parsedEagle.library.packages[0];
+
+  console.log(comp);
 }
 
-main()
+main();
